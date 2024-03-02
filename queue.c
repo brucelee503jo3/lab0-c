4ac049564958e2753ae44faffb85ae45db315e48
brucelee503jo3 <brucelee0975272947@gmail.com>
2024-03-03 07:25:50 +0800

    Complete q_new function
    Implemet q_new by test_malloc instead of malloc because of the debug message printed by test_malloc.
    If memory allocation success, then use INIT_LIST_HEAD macro in list.h to initialize node.

--- queue.c
+++ queue.c
@@ -14,7 +14,12 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head =
+        (struct list_head *) test_malloc(sizeof(struct list_head));
+    if (head != NULL) {
+        INIT_LIST_HEAD(head);
+    }
+    return head;
 }
 
 /* Free all storage used by queue */
